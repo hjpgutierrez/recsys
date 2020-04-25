ffc5f8193d66f751bbcbfdb928044f15e0591ffb
hjpgutierrez <30810809+hjpgutierrez@users.noreply.github.com>
2020-04-24 22:12:09 -0500

    Prueba de arriba hacia abajo

--- WebApiRecSys/Controllers/WeatherForecastController.cs
+++ WebApiRecSys/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApiRecSys.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cooling", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cooling", "Mild", "Warm", "Balmy", "Hotting", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
